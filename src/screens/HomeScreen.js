173e57028b42d08bf31fab6cf7870bb9b8f2438a
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2023-07-22 00:28:06 +0300

    Add buttons for navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi people!</Text>;
-};
+const HomeScreen = ({ navigation }) => {
+
+  return (
+  <View>
+    <TouchableOpacity style={styles.buttonStyle} onPress={() => 
+    navigation.navigate("Components")
+    }>
+      <Text style={styles.textStyle}>Components Demo</Text> 
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.buttonStyle} onPress={() => 
+    navigation.navigate("List")
+    }>
+      <Text style={styles.textStyle}>List Demo</Text> 
+    </TouchableOpacity>
+  </View>
+  )};
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+  buttonStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    backgroundColor: '#4CAF50',
   },
+  textStyle: {
+      color: 'white',
+      fontSize: 20
+  }
 });
 
 export default HomeScreen;
